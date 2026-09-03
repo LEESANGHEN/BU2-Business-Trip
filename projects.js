e9f5efba65a1b3cd1cf767a0de488f35dd94f3ce
LEESANGHEN <shlee1208@intekplus.com>
2026-09-03 14:48:40 +0900

    Add files via upload

--- projects.js
+++ projects.js
@@ -217,7 +217,7 @@ function renderProjectRow(mp){
   var setupLbl=(mp.setupStart&&mp.setupEnd)?(fmtFull(mp.setupStart)+' ~ '+fmtFull(mp.setupEnd)):'-';
   var shipLbl=mp.shipDate?fmtFull(mp.shipDate):'-';
   var custReqShipLbl=mp.customerReqShipDate?fmtFull(mp.customerReqShipDate):'-';
-  var unitLbl=[mp.prodUnit,mp.customerUnit].filter(Boolean).join(' / ');
+  var unitLbl=(mp.prodUnit&&mp.customerUnit)?(mp.prodUnit+'('+mp.customerUnit+')'):(mp.prodUnit||mp.customerUnit||'');
   var admin=_isAdminMode();
   return '<tr class="pm-person-row"'+(admin?' style="cursor:pointer" onclick="openEditMasterProject(\''+mp.id+'\')"':'')+'>'
     +'<td>'+_esc(mp.category||'')+'</td>'
